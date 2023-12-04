e98e890f89c6e2ec9acecea801987fdaa2da3176
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2023-12-04 16:07:56 +0200

    step 1 and step 2 - done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,83 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleUpdate = e => {
+    const { name } = e.target;
+
+    switch (name) {
+      case 'good':
+        this.setState(prevState => ({ good: prevState.good + 1 }));
+        break;
+
+      case 'neutral':
+        this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+        break;
+
+      case 'bad':
+        this.setState(prevState => ({ bad: prevState.bad + 1 }));
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    if (total === 0) {
+      return 0;
+    }
+    const percentage = (good / total) * 100;
+    return Math.floor(percentage);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h1>Please leave feedback</h1>
+        <button name="good" onClick={this.handleUpdate}>
+          Good
+        </button>
+        <button name="neutral" onClick={this.handleUpdate}>
+          Neutral
+        </button>
+        <button name="bad" onClick={this.handleUpdate}>
+          Bad
+        </button>
+
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Positive feedback: {positivePercentage}%</p>
+      </div>
+    );
+  }
+}
